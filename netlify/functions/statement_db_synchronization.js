9b4bf05005666bd1aecd43ab7c4a5f0d3871ca35
Joeven <marketing@rapidcleanillawarra.com.au>
2026-02-17 05:29:48 +0800

    refactor: remove balance_check.html, test_email.html, and related Netlify functions to streamline project structure and eliminate unused code

--- netlify/functions/statement_db_synchronization.js
+++
@@ -1,3 +0,0 @@
-const { handler } = require('./statement_sync/statement_db_synchronization');
-
-module.exports = { handler };
--- netlify/functions/statement_file_generation.js
+++
@@ -1,3 +0,0 @@
-const { handler } = require('./statement_sync/statement_file_generation');
-
-module.exports = { handler };
